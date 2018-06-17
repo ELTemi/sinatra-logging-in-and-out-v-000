17063d8953a56e564a5be48caa6e576735ecd749
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-17 19:53:22 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,8 +2,9 @@ class Helpers < ActiveRecord::Base
 
   def self.current_user(session)
     binding.pry
-    self.is_logged_in?
+    if self.is_logged_in?
       @user = User.new
+    end
   end
 
   def self.is_logged_in?(session)
