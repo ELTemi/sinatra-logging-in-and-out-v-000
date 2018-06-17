f3dcfcbc3c8d4f3d36e51d00dc85209c18de94b7
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-17 21:10:23 +0000

    automatically backed up by learn

--- db/migrate/20180617210908_helpers.rb
+++ db/migrate/20180617210908_helpers.rb
@@ -2,7 +2,7 @@ class Helpers < ActiveRecord::Migration[5.1]
   def change
     create_table :helpers do |t|
       t.string :current_user
-      t.boolean :is_logged_in?
+      t.boolean :is_logged_in
     end
   end
 end
