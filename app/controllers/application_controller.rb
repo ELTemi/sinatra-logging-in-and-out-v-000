0b2604032182633941c5e3d710659df5efd5f0e3
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-17 20:22:16 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
     if @user != nil
       session[:user_id] = @user.id
       redirect '/account'
-    elsif @user.name != params["username"] || @user.id != session[:user_id]
+    else
       redirect '/error'
     end
   end
